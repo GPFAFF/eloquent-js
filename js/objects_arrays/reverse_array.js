aebaed0776cac682dff64696b29d36f604d1b243
Greg Pfaff <greg.pfaff@razorfish.com>
2016-02-19 16:57:24 -0600

    adding eljs solution. produces same results

--- js/objects_arrays/reverse_array.js
+++ js/objects_arrays/reverse_array.js
@@ -25,6 +25,18 @@ var reverseArrayInPlace = function(array) {
 	}
 	//console.log('results ', results);
 	return results;
+
+	/* ELOQUENT JS SOLUTION
+
+	arLen = array.length;
+
+	for(var i = 0; i < Math.floor(arLen / 2);i++) {
+		var old = array[i];
+		array[i] = array[arLen - 1 - i];
+		array[arLen - 1 - i] = old;
+	}
+	//console.log('results ', results);
+	return array;*/
 };
 
 
@@ -33,7 +45,7 @@ var reverseArrayInPlace = function(array) {
 
 console.log(reverseArray(["A", "B", "C"]));
 // → ["C", "B", "A"];
-var arrayValue = [1, 2, 3, 4, 5];
+var arrayValue = [1, 2, 3, 4, 5, 4545, 5454, 5454,5, 6, 3, 6, 35353];
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
 // → [5, 4, 3, 2, 1]

910f07c4dfc2b1a1648b39f108f09815abd3c1e8
Greg Pfaff <greg.pfaff@razorfish.com>
2016-02-19 17:00:50 -0600

    adding console.logs

---
+++ js/objects_arrays/deep_equal.js
@@ -0,0 +1,15 @@
+//Deep comparison
+
+var deepEqual = function(val, props) {
+	if (val === val) {
+		return true;
+	}
+}
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true

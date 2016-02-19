d737e80b38102f1a0698f29cfc4e8c977dea5035
Greg Pfaff <greg.pfaff@razorfish.com>
2016-02-19 13:46:05 -0600

    adding function solution for beancounting problem

--- js/functions/beanCounting.js
+++ js/functions/beanCounting.js
@@ -1,1 +1,37 @@
 //Bean Counting 
+
+var countBs = function(string) {
+	var upperCount = 0;
+
+	return countChar(string, 'B');
+
+	/*for (var i = 0, len = string.length; i < len; i++) {
+		strings = string.charAt(i);
+		console.log('strings ', strings)
+
+		if(strings === "B"){
+			console.log('stings Upper ', strings.toUpperCase());
+			upperCount += 1;
+		}
+	}
+	return upperCount;*/
+}
+
+var countChar = function(string, charCounted) {
+	var count = 0;
+
+	for(var i = 0, len = string.length; i < len; i++) {
+		strings = string.charAt(i);
+
+		console.log('strings ', strings)
+
+		if(strings === charCounted) {
+			count += 1;
+		}
+	}
+	return count;
+}
+
+
+console.log(countBs('ccd'));
+console.log(countChar('kakkerlak', 'k'));
